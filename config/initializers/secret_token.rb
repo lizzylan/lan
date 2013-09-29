a30372f1d2bd79affaad70365bb54b70118d660d
Lizzylan <lizzylan@gmail.com>
2013-09-29 13:30:58 +0800

    lan

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lizzy::Application.config.secret_key_base = '89233451ee2c3ce3ba46f175bdb46ee17a43e6db6aa9e2d3bd1f7c266634deaefb4e9bc8fc1f1c84a58478e1ceb1764b4999f006d5ba49889aa5f78e04fecc34'
